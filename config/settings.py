d5c00ce595757e5f24682b3f020755f3cb1463ff
Lotfi <lotfi.m.bendiaf@gmail.com>
2022-04-27 00:57:07 +0100

    STATICFILES_STORAGE boto3

--- config/settings.py
+++ config/settings.py
@@ -168,7 +168,8 @@ AWS_STORAGE_BUCKET_NAME='kiddilycare-bucket'
 
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
